7494e3231279b3e2866c5a12a7c68dcf65f53f59
wuwenxing <1428560819@qq.com>
2022-12-04 12:24:48 +0800

    拆分

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from "vue";
 import { App } from "./App";
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter } from "vue-router";
 import routes from "./routes/index";
+import { history } from "./routes/history";
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
+const router = createRouter({ history, routes });
 
 const app = createApp(App);
 app.use(router);
