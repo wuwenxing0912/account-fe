ed5528dbecd0c59b6a4ff2f804efa0f52b9958d2
wuwenxing <1428560819@qq.com>
2022-12-05 21:30:38 +0800

    调试样式

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import "./App.scss";
 
 export const App = defineComponent({
   setup() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
     return () => (
       <div>
         <RouterView />
