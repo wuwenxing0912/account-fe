e6837617f370110b9a5c1bbe03fad30c7d628d5b
wuwenxing <1428560819@qq.com>
2022-12-06 13:20:59 +0800

    添加跳过功能

--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -1,5 +1,5 @@
 import { defineComponent } from "vue";
-import { RouterView } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
 import style from "./style/welcome.module.scss";
 import logo from "../../assets/logo.svg";
 
@@ -7,6 +7,9 @@ export default defineComponent({
   setup() {
     return () => (
       <div class={style["welcome-container"]}>
+        <div class={style["skip-welcome"]}>
+          <RouterLink to="/start">跳过</RouterLink>
+        </div>
         <header>
           <img src={logo} class={style["welcome-logo"]} />
           <span class={style["logo-text"]}>薪水去哪儿了</span>
