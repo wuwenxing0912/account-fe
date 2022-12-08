086989e99697bb2e56f26248fae27a76914b9db0
wuwenxing <1428560819@qq.com>
2022-12-08 21:05:31 +0800

    尝试

--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -42,3 +42,30 @@ export default defineComponent({
     );
   },
 });
+// const route = useRoute();
+// const currentRoutePath = ref<string>("");
+// const historyRoutePath = ref<string>("");
+// currentRoutePath.value = route.path;
+// historyRoutePath.value = route.path;
+// const enterClass = ref<string>("slide_fade_enter_from");
+// const leaveClass = ref<string>("slide_fade_leave_to");
+// const onBeforeEnter = (r: RouteLocationNormalizedLoaded) => {
+//   historyRoutePath.value = currentRoutePath.value;
+//   currentRoutePath.value = r.path;
+//   const historyRouteIndex = getRouteIndex(historyRoutePath.value);
+//   const currentRouteIndex = getRouteIndex(currentRoutePath.value);
+//   if (currentRouteIndex > historyRouteIndex) {
+//     enterClass.value = "slide_fade_enter_from";
+//     leaveClass.value = "slide_fade_leave_to";
+//   } else {
+//     enterClass.value = "slide_fade_enter_from_back";
+//     leaveClass.value = "slide_fade_leave_to_back";
+//   }
+// };
+// const getRouteIndex = (path: string) => {
+//   if (!path) {
+//     return 0;
+//   }
+//   const routeArray = path.split("/");
+//   return parseInt(routeArray[routeArray.length - 1]);
+// };
